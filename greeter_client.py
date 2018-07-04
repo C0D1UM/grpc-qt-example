6e6051c0077fe92bcb7ed0337c5ccb8b35bebfea
Sivabudh Umpudh <sivabudh.u@codium.co>
2018-07-04 16:52:41 +0700

    Python client is done

---
+++ greeter_client.py
@@ -0,0 +1,13 @@
+import grpc
+
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+def run():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = helloworld_pb2_grpc.GreeterStub(channel)
+        response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+    print("Greeter client received: " + response.message)
+
+if __name__ == '__main__':
+    run()
