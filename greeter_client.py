83ff2984014cb1d94514912c81e58ddc89cdb8bd
Sivabudh Umpudh <sivabudh.u@codium.co>
2018-07-04 16:56:02 +0700

    Moved to 50060 port number

--- greeter_client.py
+++ greeter_client.py
@@ -4,7 +4,7 @@ import helloworld_pb2
 import helloworld_pb2_grpc
 
 def run():
-    with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel('localhost:50060') as channel:
         stub = helloworld_pb2_grpc.GreeterStub(channel)
         response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
     print("Greeter client received: " + response.message)
--- greeter_server.cc
+++ greeter_server.cc
@@ -25,7 +25,7 @@ class GreeterServiceImpl final : public Greeter::Service {
 };
 
 void RunServer() {
-  std::string server_address("0.0.0.0:50051");
+  std::string server_address("0.0.0.0:50060");
   GreeterServiceImpl service;
 
   ServerBuilder builder;
